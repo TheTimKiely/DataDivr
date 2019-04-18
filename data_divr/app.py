7e5e5eb5eb7f5af2baaf8b69d28d24c7d567decf
Tim Kiely <tkiely@veic.org>
2019-04-18 12:16:59 -0400

    changed port

--- data_divr/app.py
+++ data_divr/app.py
@@ -24,6 +24,6 @@ def get_tasks():
     return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(debug=True)
+    #app.debug = True
+    #app.run(debug=True)
     app.run(host="0.0.0.0", port=80)
