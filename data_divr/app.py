75890b1bbe539c693a0f32305e518a4acf44afd5
Tim Kiely <tkiely@veic.org>
2019-04-18 12:16:02 -0400

    changed port

--- data_divr/app.py
+++ data_divr/app.py
@@ -26,3 +26,4 @@ def get_tasks():
 if __name__ == '__main__':
     app.debug = True
     app.run(debug=True)
+    app.run(host="0.0.0.0", port=80)
--- ec2__setup.sh
+++ ec2__setup.sh
@@ -1,4 +1,3 @@
 sudo apt-get update
 sudo apt-get install python3-pip
-cd recurrent-neural-networks
 pip3 install --user -r requirements.txt
